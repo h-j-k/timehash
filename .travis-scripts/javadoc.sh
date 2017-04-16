9ad59f4f750ba0d9e924dda50b7db8936759745a
h-j-k <han.junkwang@gmail.com>
2017-04-16 23:46:40 +0800

    updated travis-ci script to use gradle for this project.

--- .travis-scripts/javadoc.sh
+++ .travis-scripts/javadoc.sh
@@ -8,7 +8,7 @@ jdb=gh-pages
 websrc=$TRAVIS_BUILD_DIR/target/site/apidocs
 webtgt=$(mktemp -d)
 [ -z "$webtgt" ] && lgm "Error creating tmp dir, bailing." && exit 1
-mvn javadoc:javadoc
+gradle javadoc
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 lgm Cloning...
